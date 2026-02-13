8efde599c45cdfa21353039b94070a6d86da8a5a
scroll2learn <73377680+scroll2learn@users.noreply.github.com>
2026-02-14 00:38:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    who = "Whizlabs"
+    who = "Herolabs"
     if request.method == "POST":
-        who = request.form.get("who", "Whizlabs")
+        who = request.form.get("who", "Herolabs")
     return render_template("index.html", who=who)
 
 if __name__ == "__main__":
